2fc5331ce91e38fb6580b91dcced5f136f308dde
franvasquez9 <89528074+franvasquez9@users.noreply.github.com>
2022-06-29 18:50:23 -0500

    ejercicio 5 JS

---
+++ ejercicio5.js
@@ -0,0 +1,9 @@
+alturaCm = 166;
+alturaMt = 1.66;
+pesoKg = 63.4;
+alturaRedondeada = Math.round(alturaMt);
+console.log(alturaRedondeada);
+pesoRedondeado = Math.round(pesoKg);
+console.log(pesoRedondeado);
+let maximo_valor = Number.MAX_VALUE + 1;
+console.log(maximo_valor);
